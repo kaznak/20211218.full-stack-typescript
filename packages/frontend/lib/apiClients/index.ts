3adeafd844243e71eb525c27b954fdcef32599fb
kaznak <kaznak.at.work@gmail.com>
2021-12-22 00:36:40 +0900

    lib/apiClients/index.ts: fix

--- packages/frontend/lib/apiClients/index.ts
+++ packages/frontend/lib/apiClients/index.ts
@@ -2,8 +2,6 @@ import axios from 'axios'
 import aspida from '@aspida/axios'
 import api from 'api/$api'
 
-const baseURL =
-  (process.env.NEXT_PUBLIC_BASE_URL ? process.env.NEXT_PUBLIC_BASE_URL : '') +
-  '/api'
+const baseURL = (process.env.NEXT_PUBLIC_BASE_URL || '') + '/api'
 
 export const apiClient = api(aspida(axios, { baseURL }))
