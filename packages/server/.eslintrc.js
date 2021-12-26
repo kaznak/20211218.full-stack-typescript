3a4acb3a7df09e29513c39e9e3227e922ed02d87
kaznak <kaznak.at.work@gmail.com>
2021-12-26 15:00:11 +0900

    server/: wip

--- packages/server/.eslintrc.js
+++ packages/server/.eslintrc.js
@@ -9,7 +9,17 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
   ],
+  settings: {
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+        project: ".",
+      },
+    },
+  },
   env: {
     browser: false,
     es2021: true,
